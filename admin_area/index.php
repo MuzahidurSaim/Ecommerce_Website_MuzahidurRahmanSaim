c9613bea2c09322a12cbee2c98d362e937b3b5a5
MuzahidurSaim <muzahidur.saim@northsouth.edu>
2025-08-01 11:11:40 +0600

    finished part-8 (add Insert Brand page, style Insert Category and Brand page)

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,6 +8,9 @@
     <!-- bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- css file -->
     <link rel = "stylesheet" href = "../style.css">
     <style>
@@ -53,9 +56,9 @@
                 <div class="button text-center">
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">Insert Products</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">View Products</a></button>
-                    <button><a href="insert_categories.php" class="nav-link text-light bg-info m-1 p-2">Insert Categories</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light bg-info m-1 p-2">Insert Categories</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">View Categories</a></button>
-                    <button><a href="" class="nav-link text-light bg-info m-1 p-2">Insert Brands</a></button>
+                    <button><a href="index.php?insert_brand" class="nav-link text-light bg-info m-1 p-2">Insert Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">View Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">All Orders</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">All Payments</a></button>
@@ -63,6 +66,18 @@
                     <button><a href="" class="nav-link text-light bg-info m-1 p-2">Logout</a></button>
                 </div>
             </div>
+        </div>
+
+        <!-- fourth child -->
+        <div class="container my-5">
+            <?php
+                if(isset($_GET['insert_category'])) {
+                    include('insert_categories.php');
+                }
+                if(isset($_GET['insert_brand'])) {
+                    include('insert_brands.php');
+                }
+            ?>
         </div>
 
         <!-- last child -->
